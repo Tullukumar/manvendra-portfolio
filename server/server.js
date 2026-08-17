1168ba9a1e221bcd7a38c4ea27ad611450e314ca
Manvendra Kumar <manvendraroy1234@gmail.com>
2026-08-17 23:12:00 +0530

    Prepare backend for deployment

--- server/server.js
+++ server/server.js
@@ -49,7 +49,6 @@ app.get("/", (req, res) => {
 /* =========================
     START SERVER
 ========================= */
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
